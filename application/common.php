97b9239f67271c79d5f1b5088488c4437788cc2d
18845071802 <1763326695@qq.com>
2019-10-29 13:50:24 +0800

    api1.0第一次

---
+++ application/common.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 流年 <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 应用公共文件
+function ret($data=[],$code=0){
+    return json(['error_code'=>$code,'data'=>$data]);
+}
+function object_array($array) {
+    if(is_object($array)) {
+        $array = (array)$array;
+    }
+    if(is_array($array)) {
+        foreach($array as $key=>$value) {
+            $array[$key] =object_array($value);
+        }
+    }
+    return $array;
+}
